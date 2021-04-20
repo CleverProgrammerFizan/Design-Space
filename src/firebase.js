05c457ee94ce94f6add090d32b5c69f18bec96fa
CleverProgrammerFizan <amefizthepubgpro@gmail.com>
2021-04-20 04:16:45 -0700

    make it better

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBdksnu0BMdeH7cffGK80kV_NhaHx2PBw",
+    authDomain: "design-space-d3c6d.firebaseapp.com",
+    projectId: "design-space-d3c6d",
+    storageBucket: "design-space-d3c6d.appspot.com",
+    messagingSenderId: "240746963445",
+    appId: "1:240746963445:web:0395cab5ccfba2d58ac0bc"
+  };
